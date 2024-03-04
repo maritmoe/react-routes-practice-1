7b3c46a3b169897ce574e4cd9b32dddcfbd420cb
Marit Kristine Moe <marit.moe2@ntebb.no>
2024-03-04 09:50:15 +0100

    Exercise solved.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react';
-import { Route, Routes, Link } from 'react-router-dom';
-import { appsData, productsData } from './data.js';
-import Home from './pages/Home';
-import ProductsPage from './pages/Products';
-import './App.css';
+import { useState } from "react";
+import { Route, Routes, Link } from "react-router-dom";
+import { appsData, productsData } from "./data.js";
+import Home from "./pages/Home";
+import ProductsPage from "./pages/Products";
+import "./App.css";
+import AppsPage from "./pages/Apps";
 
 export default function App() {
-  const [products, setProducts] = useState(productsData);
-  const [apps, setApps] = useState(appsData);
+  const [products] = useState(productsData);
+  const [apps] = useState(appsData);
 
   console.log({ products, apps });
 
@@ -23,6 +24,9 @@ export default function App() {
             <li>
               <Link to="/products">Products</Link>
             </li>
+            <li>
+              <Link to="/apps">Apps</Link>
+            </li>
           </ul>
         </nav>
       </header>
@@ -32,6 +36,7 @@ export default function App() {
           element={<ProductsPage products={products} />}
         />
         <Route path="/" element={<Home />} />
+        <Route path="/apps" element={<AppsPage apps={apps} />} />
       </Routes>
     </div>
   );
